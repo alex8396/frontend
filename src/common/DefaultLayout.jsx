f38648291afa5cfa28f68fe5ce3860cca50a771a
alex8396 <alex8396@naver.com>
2025-05-22 14:48:59 +0900

    Fix layout bug in DefaultLayout.

--- src/common/DefaultLayout.jsx
+++ src/common/DefaultLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 import './index.css'
 import css from './defaultlayout.module.css'
+import { TokenChecker } from './TokenChecker' // 토큰 체커 컴포넌트 import
 
 export const DefaultLayout = () => {
   return (
